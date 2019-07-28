c1e5c01102456d2a76fe3e3dee5a3ca714b164dd
Chris Ford <chrisford51@gmail.com>
2019-07-27 20:47:37 -0400

    Removed files, added info, created people.js Component

---
+++ myapp/src/people.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class People extends Component{
+  render(){
+    return(
+      <div className="people">
+        <div>Name: Chris</div>
+        <div>Age: 24</div>
+        <div>Location: Indiana</div>
+      </div>
+    )
+  }
+}
+
+export default People
